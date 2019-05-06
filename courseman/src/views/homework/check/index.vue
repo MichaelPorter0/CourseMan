2f6e8827cdb9c747ff2efd1d99480d4b013ce21e
michale <miporterg@gmail.com>
2019-05-06 15:56:18 +0800

    m

---
+++ courseman/src/views/homework/check/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">提交作业列表</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name',
+      'roles',
+      'introduction',
+      'device',
+      'avatar'
+    ])
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
